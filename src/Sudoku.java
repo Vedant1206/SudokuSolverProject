2d27391dcb759654524df5d6ac997377462fa79c
Vedant1206 <70309263+Vedant1206@users.noreply.github.com>
2022-01-13 22:55:40 -0600

    added test files and constructor

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,5 +1,12 @@
 public class Sudoku {
-  public Sudoku(){
+  int[][] board;
+  public Sudoku(int[][] temp){
+    board = new int[temp.length][temp[0].length];
 
+    for(int i = 0; i<temp.length; i++){
+      for(int j = 0; j<temp[i].length; j++){
+        board[i][j] = temp[i][j];
+      }
+    }
   }
 }
--- src/main.java
+++ src/main.java
@@ -1,7 +1,19 @@
 public class main {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("Welcome to Sudoku");
-    
+    int[][] board = {
+        {8, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 3, 6, 0, 0, 0, 0, 0},
+        {0, 7, 0, 0, 9, 0, 2, 0, 0},
+        {0, 5, 0, 0, 0, 7, 0, 0, 0},
+        {0, 0, 0, 0, 4, 5, 7, 0, 0},
+        {0, 0, 0, 1, 0, 0, 0, 3, 0},
+        {0, 0, 1, 0, 0, 0, 0, 6, 8},
+        {0, 0, 8, 5, 0, 0, 0, 1, 0},
+        {0, 9, 0, 0, 0, 0, 4, 0, 0}
+    };
+
+    Sudoku s = new Sudoku(board);
   }
 }
