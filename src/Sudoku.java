e79c4b41bc4f86af3ab08d6c13216376196e679f
Vedant1206 <70309263+Vedant1206@users.noreply.github.com>
2022-01-14 15:22:35 -0600

    added necessary boolean functions

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -9,4 +9,36 @@ public class Sudoku {
       }
     }
   }
+  public boolean notInRow(int row, int num){
+    for(int i = 0; i<board.length; i++){
+      if(board[row][i] == num){
+        return true;
+      }
+    }
+    return false;
+  }
+  public boolean notInCol(int col, int num){
+    for(int i = 0; i<board.length; i++){
+      if(board[i][col] == num){
+        return true;
+      }
+    }
+    return false;
+  }
+  private boolean notInBox(int row, int col, int num) {
+    int squareRoot = (int)Math.sqrt(board.length);
+    int newRow = row - row%(squareRoot);
+    int newCol = col - col%(squareRoot);
+
+    for (int i = newRow; i<newRow+3; i++) {
+      for (int j = newCol; j<newCol+3; j++) {
+        if (board[newRow][newCol]==num) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+
 }
